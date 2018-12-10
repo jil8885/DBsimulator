2a94a3aac7deda05ae9a282f52556aa503c69986
이정인 <jil8885@gmail.com>
2018-12-10 17:30:12 +0900

    remove while clause in python

--- sql_update.py
+++ sql_update.py
@@ -13,26 +13,24 @@ def execute():
     db = client.hotel
     temperature = db.room_temperature
     positiondb = db.employee_position
-    while True:
-        cursor.execute("select room_number, state_in_room from room_information where state_transaction = 1")
-        rooms = cursor.fetchall()
-        for room in random.sample(rooms, len(rooms) // 3):
-            if room[1] == 1:
-                cursor.execute("update room_information set state_in_room = 0 where room_number = " + str(room[0]))
-            else:
-                cursor.execute("update room_information set state_in_room = 1 where room_number = " + str(room[0]))
-        connect.commit()
-        var = [-0.5 , -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5]
-        for x in range(1, 16):
-            for y in range(1, 11):
-                room_num = x * 100 + y
-                room_temp = temperature.find_one({"room" : room_num})["temperature"]
-                room_humi = temperature.find_one({"room" : room_num})["humidity"]
-                var_temp = random.sample(var, 1)[0]
-                var_humi = random.sample(var, 1)[0]
-                temperature.update_one({"room" : room_num}, {"$set" :{"temperature" : room_temp + var_temp, "humidity" : room_humi + var_humi}} ,True)
-        for x in range(500):
-            positiondb.update_one({"id" : x}, {"$set": {"position": random.randint(0, 15)}}, True)
-        time.sleep(20)
+    cursor.execute("select room_number, state_in_room from room_information where state_transaction = 1")
+    rooms = cursor.fetchall()
+    for room in random.sample(rooms, len(rooms) // 3):
+        if room[1] == 1:
+            cursor.execute("update room_information set state_in_room = 0 where room_number = " + str(room[0]))
+        else:
+            cursor.execute("update room_information set state_in_room = 1 where room_number = " + str(room[0]))
+    connect.commit()
+    var = [-0.5 , -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5]
+    for x in range(1, 16):
+        for y in range(1, 11):
+            room_num = x * 100 + y
+            room_temp = temperature.find_one({"room" : room_num})["temperature"]
+            room_humi = temperature.find_one({"room" : room_num})["humidity"]
+            var_temp = random.sample(var, 1)[0]
+            var_humi = random.sample(var, 1)[0]
+            temperature.update_one({"room" : room_num}, {"$set" :{"temperature" : room_temp + var_temp, "humidity" : room_humi + var_humi}} ,True)
+    for x in range(500):
+        positiondb.update_one({"id" : x}, {"$set": {"position": random.randint(0, 15)}}, True)
 execute()
     
